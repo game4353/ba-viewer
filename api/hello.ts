9315f4ad9610e4c18febbf91a3288fc4fd7846d9
game4353 <73753515+game4353@users.noreply.github.com>
2024-05-18 10:57:05 +0800

    fs

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,9 @@
 type VercelRequest = import("@vercel/node").VercelRequest;
 type VercelResponse = import("@vercel/node").VercelResponse;
+const fs = require("fs");
 
 module.exports = function handler(req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query;
-  return res.json({
-    message: `Hello ${name}!`,
-  });
+  const j = fs.readFileSync(`./src/assets/game/excel/${name}.json`);
+  return res.json(j);
 };
