cbb2dfc937f60ce90e433501f08b8c1a0aebf53c
game4353 <73753515+game4353@users.noreply.github.com>
2024-05-18 11:18:38 +0800

    world

--- api/world.ts
+++ api/world.ts
@@ -3,7 +3,13 @@ type VercelResponse = import("@vercel/node").VercelResponse;
 const fs = require("fs");
 
 module.exports = function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = "academyfavorscheduleexceltable" } = req.query;
-  const j = fs.readFileSync(`../src/assets/game/excel/${name}.json`);
+  const { name = "World" } = req.query;
+  let path: string;
+  if (Array.isArray(name)) {
+    path = name[0].split("_").join("/");
+  } else {
+    path = name.split("_").join("/");
+  }
+  const j = fs.readFileSync(path);
   return res.json(j);
 };
