f763fea7ac4490f69a2073eb4d18ec3901345f98
game4353 <73753515+game4353@users.noreply.github.com>
2024-05-18 17:22:16 +0800

    demo send tsv

--- api/demo.js
+++ api/demo.js
@@ -4,11 +4,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const path = require("path");
 const fs_1 = require("fs");
+function jsonToTsv(jsonString) {
+    const excel = JSON.parse(jsonString);
+    const jsonArray = excel.DataList;
+    if (!Array.isArray(jsonArray))
+        return null;
+    if (jsonArray.length === 0)
+        return "";
+    const headers = Object.keys(jsonArray[0]);
+    const tsvRows = jsonArray.map((row) => {
+        return headers
+            .map((header) => {
+            var _a, _b;
+            const value = row[header];
+            return (_b = (_a = value === null || value === void 0 ? void 0 : value.toString()) === null || _a === void 0 ? void 0 : _a.replace(/\t/g, " ")) !== null && _b !== void 0 ? _b : "";
+        })
+            .join("\t");
+    });
+    return `${headers.join("\t")}\n${tsvRows.join("\n")}`;
+}
 function handler(req, res) {
-    const { name = "SpineCharacters" } = req.query;
-    const folder = path.join(process.cwd(), "src/assets/game");
+    const { name = "" } = req.query;
+    const folder = path.join(process.cwd(), "src/assets/game/excel");
     const file = path.join(folder, name + ".json");
     const json = (0, fs_1.readFileSync)(file, { encoding: "utf-8" });
-    return res.json(JSON.parse(json));
+    const tsv = jsonToTsv(json);
+    return res.send(tsv);
 }
 exports.default = handler;
--- src/api/demo.ts
+++ src/api/demo.ts
@@ -5,10 +5,28 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import * as path from "path";
 import { readFileSync } from "fs";
 
+function jsonToTsv(jsonString: string): string | null {
+  const excel = JSON.parse(jsonString);
+  const jsonArray = excel.DataList;
+  if (!Array.isArray(jsonArray)) return null;
+  if (jsonArray.length === 0) return "";
+  const headers = Object.keys(jsonArray[0]);
+  const tsvRows = jsonArray.map((row) => {
+    return headers
+      .map((header) => {
+        const value = row[header];
+        return value?.toString()?.replace(/\t/g, " ") ?? "";
+      })
+      .join("\t");
+  });
+  return `${headers.join("\t")}\n${tsvRows.join("\n")}`;
+}
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = "SpineCharacters" } = req.query;
-  const folder = path.join(process.cwd(), "src/assets/game");
+  const { name = "" } = req.query;
+  const folder = path.join(process.cwd(), "src/assets/game/excel");
   const file = path.join(folder, name + ".json");
   const json = readFileSync(file, { encoding: "utf-8" });
-  return res.json(JSON.parse(json));
+  const tsv = jsonToTsv(json);
+  return res.send(tsv);
 }
