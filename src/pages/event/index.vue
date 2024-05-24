c3ebcd06b6772adb0508cbe8f19e9e1c6a2ae0c1
game4353 <73753515+game4353@users.noreply.github.com>
2024-05-24 22:34:47 +0800

    add: /event

--- src/pages/event/index.vue
+++ src/pages/event/index.vue
@@ -1,10 +1,52 @@
-<template>{{}}</template>
+<template>
+  <v-tabs
+    class="mb-2"
+    v-model="tab"
+    align-tabs="center"
+    color="deep-purple-accent-4"
+  >
+    <v-tab v-for="(v, i) of Object.keys(tabs)" :key="i" :value="i">{{
+      v
+    }}</v-tab>
+  </v-tabs>
+
+  <v-tabs-window v-model="tab">
+    <v-tabs-window-item
+      v-for="(v, i) of Object.values(tabs)"
+      :key="i"
+      :value="i"
+    >
+      <EventList :events="v" />
+    </v-tabs-window-item>
+  </v-tabs-window>
+</template>
 
 <script setup lang="ts">
 import type { EventContentSeasonExcel } from "~game/types/flatDataExcel";
 // @ts-ignore
 import { DataList } from "~game/excel/EventContentSeasonExcelTable.json";
 
+const tab = ref(0);
 const events = DataList as EventContentSeasonExcel[];
-console.log(events);
+
+const normal: EventContentSeasonExcel[] = [];
+const rerun: EventContentSeasonExcel[] = [];
+const forever: EventContentSeasonExcel[] = [];
+const mini: EventContentSeasonExcel[] = [];
+
+const tabs = {
+  初回: normal,
+  復刻: rerun,
+  常設: forever,
+  総決算: mini,
+};
+
+events.forEach((v) => {
+  const type = v.EventContentType;
+  if (type === "MiniEvent") mini.push(v);
+  if (type !== "Stage") return;
+  if (v.EventContentCloseTime.startsWith("2099")) forever.push(v);
+  else if (!v.IsReturn) normal.push(v);
+  else rerun.push(v);
+});
 </script>
