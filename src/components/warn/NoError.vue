e787701d140c856e50e4a350b26eec75a7d7b4f7
game4353 <73753515+game4353@users.noreply.github.com>
2024-05-31 16:47:26 +0800

    add INJECT_ERR_FILTER_UNIQUE

--- src/components/warn/NoError.vue
+++ src/components/warn/NoError.vue
@@ -4,8 +4,15 @@
 </template>
 
 <script setup lang="ts">
-import { INJECT_ERR, err, setError } from "@/utils/error";
+import {
+  INJECT_ERR,
+  INJECT_ERR_FILTER_UNIQUE,
+  err,
+  filterUniqueOrError,
+  setError,
+} from "@/utils/error";
 import { provide } from "vue";
 
 provide(INJECT_ERR, setError);
+provide(INJECT_ERR_FILTER_UNIQUE, filterUniqueOrError);
 </script>
--- src/utils/error.ts
+++ src/utils/error.ts
@@ -17,4 +17,35 @@ export function setError(message: string) {
  * ```setError(`${something} is missing.`)```
  *
  */
-export const INJECT_ERR: InjectionKey<typeof setError> = Symbol("inject err");
+export const INJECT_ERR: InjectionKey<typeof setError> = Symbol("INJECT_ERR");
+
+type KeyValuePairs<T> = {
+  [K in keyof T]: [K, T[K]];
+}[keyof T][];
+
+export function filterUniqueOrError<T>(
+  listName: string,
+  list: T[],
+  kvs: KeyValuePairs<T>,
+): T {
+  let res = list;
+  for (const [k, v] of kvs) res = res.filter((obj) => obj[k] === v);
+  if (res.length === 1) return res[0];
+
+  const conds = kvs.map(([k, v]) => `${String(k)}=${v}`).join(", ");
+  const message = `There are ${res.length} results of ${listName} (${conds}).`;
+  setError(message);
+  return null as any;
+}
+
+/**
+ * Usage:
+ *
+ * `const filterUniqueOrError = inject(INJECT_ERR_FILTER_UNIQUE)!;`
+ *
+ * `const item = filterUniqueOrError("List", list, [[key1, val1], [key2, val2]]);`
+ *
+ */
+export const INJECT_ERR_FILTER_UNIQUE: InjectionKey<
+  typeof filterUniqueOrError
+> = Symbol("INJECT_ERR_FILTER_UNIQUE");
