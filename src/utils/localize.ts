445ce5fabc56b736e38b583ec595eed700dadc61
game4353 <73753515+game4353@users.noreply.github.com>
2024-06-01 11:29:26 +0800

    add LocalizeExcel

--- src/utils/localize.ts
+++ src/utils/localize.ts
@@ -1,11 +1,19 @@
 // @ts-ignore
-import type { LocalizeEtcExcel } from "~game/types/flatDataExcelDb";
+import type {
+  LocalizeEtcExcel,
+  LocalizeExcel,
+} from "~game/types/flatDataExcelDb";
+import { default as a0 } from "~game/db/ExcelDB/Localize.json";
 import { default as a1 } from "~game/db/ExcelDB/LocalizeEtc.json";
 import { unreachable } from "./misc";
 
 type DB<T> = {
   Bytes: T;
 }[];
+
+const localizeDict = Object.fromEntries(
+  (a0 as DB<LocalizeExcel>).map((o) => [o.Bytes.Key, o.Bytes]),
+);
 
 const etcDict = Object.fromEntries(
   (a1 as DB<LocalizeEtcExcel>).map((o) => [o.Bytes.Key, o.Bytes]),
@@ -20,4 +28,23 @@ export function etc(id: number, desc = false, lang: "JP" | "KR" = "JP") {
   if (lang == "JP") return desc ? o.DescriptionJp : o.NameJp;
   if (lang == "KR") return desc ? o.DescriptionKr : o.NameKr;
   unreachable();
+}
+
+export class Localize {
+  static lang: "JP" | "KR" = "JP";
+
+  static setLang(lang: "JP" | "KR") {
+    Localize.lang = lang;
+  }
+
+  static localize(id: number) {
+    const o = localizeDict[String(id)];
+    if (o == null) {
+      console.error(id);
+      return "�";
+    }
+    if (Localize.lang === "JP") return o.Jp;
+    if (Localize.lang === "KR") return o.Kr;
+    unreachable();
+  }
 }
