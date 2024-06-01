2b4a77afb923cf1a5e9571d04aa9b760f8c36bbe
game4353 <73753515+game4353@users.noreply.github.com>
2024-06-01 22:43:49 +0800

    add Icon for Atlas

---
+++ src/components/GameImg/icon.ts
@@ -0,0 +1,17 @@
+import Outdoor from "~assets/Atlas/Common/Common_Icon_Outdoor.png";
+import Indoor from "~assets/Atlas/Common/Common_Icon_Indoor.png";
+import Street from "~assets/Atlas/Common/Common_Icon_Street.png";
+import BgN from "~assets/Atlas/Common/Card_Item_Bg_N.png";
+import BgR from "~assets/Atlas/Common/Card_Item_Bg_R.png";
+import BgSR from "~assets/Atlas/Common/Card_Item_Bg_SR.png";
+import BgSSR from "~assets/Atlas/Common/Card_Item_Bg_SSR.png";
+
+export const Icon = {
+  Outdoor,
+  Indoor,
+  Street,
+  BgN,
+  BgR,
+  BgSR,
+  BgSSR,
+};
