ebd876f8db1496170ee3053e8703b032ba5057c9
game4353 <73753515+game4353@users.noreply.github.com>
2024-11-25 14:32:34 +0800

    add a filter panel in /student

--- src/pages/student/index.vue
+++ src/pages/student/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="h-full">
-    <Scroll class="mx-6 px-2">
-      <StudentList :character-ids="studentIds" />
-    </Scroll>
+  <div class="flex flex-row h-full">
+    <div class="w-[240px] h-full flex-shrink-0">
+      <FilterPanel :items="students" :setOrder :setVisible />
+    </div>
+    <div class="h-full">
+      <Scroll class="mx-6 px-2">
+        <StudentList :characterIds="studentIds" :visibleIds :orderMap />
+      </Scroll>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { usePlayableIds } from "@/components/parcel/character/character";
+import {
+  CCharacter,
+  useCharacter,
+  usePlayableIds,
+} from "@/components/parcel/character/character";
+import { fail } from "@/utils/misc";
 
 const playableIds = usePlayableIds();
 const studentIds = computed(() => {
@@ -15,4 +25,17 @@ const studentIds = computed(() => {
   if (playableIds.value.isErr()) return [];
   return playableIds.value.unwrap();
 });
+const students = computed(() =>
+  studentIds.value
+    .map((id) => useCharacter(id).value?.unwrapOrElse(fail))
+    .filter((v): v is CCharacter => v != null),
+);
+const orderMap = ref<Map<number, number>>();
+const visibleIds = ref<Set<number>>();
+function setOrder(map: Map<number, number>) {
+  orderMap.value = map;
+}
+function setVisible(set: Set<number>) {
+  visibleIds.value = set;
+}
 </script>
